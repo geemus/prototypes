9d49f33d195dbc0730c1fa09db3333ef50a9c944
geemus (Wesley Beary) <wbeary@engineyard.com>
2010-04-07 09:37:55 -0700

    cleanup output/api for tach

--- tach.rb
+++ tach.rb
@@ -1,55 +1,71 @@
-# TODO: add tachs above tach, allows setting describes/counts that are default but can be over-ridden?
-
 require 'rubygems'
 require 'formatador'
 
 def run_in_thread(name, count, benchmark)
   thread = Thread.new {
     thread_start = Time.now
-    Formatador.display_line(name)
-    Formatador.redisplay_progressbar(0, count, :started_at => thread_start)
+    Formatador.redisplay_progressbar(0, count, :label => name, :started_at => thread_start)
     count.times do |index|
       tach_start = Time.now
       instance_eval(&benchmark)
       tach_elapsed = Time.now.to_f - tach_start.to_f
       Thread.current[:results] ||= []
       Thread.current[:results] << tach_elapsed
-      Formatador.redisplay_progressbar(index + 1, count, :started_at => thread_start)
+      Formatador.redisplay_progressbar(index + 1, count, :label => name, :started_at => thread_start)
     end
+    Formatador.display_line
   }
   thread.join
   thread[:results]
 end
 
 @benchmarks = []
-def tach(name, count = 1, &block)
-  @benchmarks << [name, count, block]
+@names = []
+@results = {}
+@times = 0
+
+def tachs(times = 1, &block)
+  @times = times
+  instance_eval(&block)
+  Formatador.display_line
+
+  @benchmarks.each do |name, count, benchmark|
+    @names << name
+  end
+
+  longest = @names.map {|name| name.length}.max
+
+  @benchmarks.each do |name, count, benchmark|
+    @results[name] = run_in_thread("#{name}#{' ' * (longest - name.length)}", count, benchmark)
+  end
+
+  data = []
+  @names.each do |name|
+    value = @results[name]
+    total = value.inject(0) {|sum,item| sum + item}
+    data << { :average => format("%.5f", (total / value.length)), :tach => name, :total => format("%.5f", total) }
+  end
+
+  Formatador.display_table(data, [:tach, :average, :total])
+  Formatador.display_line
+end
+
+def tach(name, &block)
+  @benchmarks << [name, @times, block]
 end
 
 require 'uri'
 
 srand
 
-tach('first', 4) do
-  sleep(rand)
-end
+tachs(4) do
 
-tach('second', 4) do
-  sleep(rand)
-end
+  tach('first') do
+    sleep(rand)
+  end
 
-@names = []
-@results = {}
-@benchmarks.each do |name, count, benchmark|
-  @names << name
-  @results[name] = run_in_thread(name, count, benchmark)
-end
+  tach('second') do
+    sleep(rand)
+  end
 
-data = []
-@names.each do |name|
-  value = @results[name]
-  total = value.inject(0) {|sum,item| sum + item}
-  data << { :average => format("%.5f", (total / value.length)), :tach => name, :total => format("%.5f", total) }
 end
-
-Formatador.display_table(data, [:tach, :average, :total])
