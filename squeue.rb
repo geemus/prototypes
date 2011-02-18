35056c60740d69d1b146d0423c1463bf205988bb
geemus <geemus@gmail.com>
2011-02-17 20:57:01 -0800

    naive queue thing

---
+++ squeue.rb
@@ -0,0 +1,38 @@
+# someday/maybe: visibility timeouts, maybe #{created_at}.#{data[:id]}.#{locked_at}
+# someday/maybe: peek
+
+require 'rubygems'
+require 'fog'
+require 'json'
+
+class Squeue
+
+  def initialize(config = {})
+    namespace = config.delete(:namespace)
+    storage = Fog::Storage.new(config)
+    @directory = storage.directories.get(namespace) || storage.directories.create(:key => namespace)
+  end
+
+  def pop
+    file = @directory.files.all.first
+    data = JSON.parse(file.body)
+    file.destroy
+    data
+  end
+
+  def push(data)
+    key = Time.now.to_i.to_s << '.' << data[:id]
+    @directory.files.create(
+      :key  => key,
+      :body => data.to_json
+    )
+    key
+  end
+
+end
+
+if __FILE__ == $0
+  squeue = Squeue.new(:namespace => 'squeue', :provider => 'AWS')
+  p squeue.push(:id => 'foo', :do => :work)
+  p squeue.pop
+end
