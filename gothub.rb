aa6819e0b57d2a32345a828ff7ca606fe27e83f5
geemus <geemus@gmail.com>
2011-05-01 14:41:05 -0700

    messing with more repository methods

--- gothub.rb
+++ gothub.rb
@@ -47,14 +47,6 @@ end
 
 class Collection
 
-  def self.model(new_model=nil)
-    unless new_model
-      @model
-    else
-      @model = new_model
-    end
-  end
-
   def self.path(new_path=nil)
     unless new_path
       @path
@@ -70,11 +62,11 @@ class Collection
   end
 
   def request_many(options)
-    self.class.model.many(request(options))
+    Model.many(request(options))
   end
 
   def request_one(options)
-    self.class.model.one(request(options))
+    Model.one(request(options))
   end
 
   private
@@ -88,24 +80,53 @@ end
 
 class Repos < Collection
 
-  class Repo < Model; end
-  model(Repo)
   path('/api/v2/json/repos')
 
   def all(username)
     request_many(:path => "/show/#{username}")
   end
 
-  def get(username, key)
-    request_one(:path => "/show/#{username}/#{key}")
+  def create(repo, data)
+    query = { :name => repo }.merge(data)
+    data.each do |key, value|
+      query["values[#{key}]"] = value
+    end
+    request_one(:method => :post, :path => "/create", :query => query)
+  end
+
+  def delete(username, repo, delete_token=nil)
+    query = {}
+    if delete_token
+      query[:delete_token] = delete_token
+    end
+    request_one(:method => :post, :path => "/delete/#{username}/#{repo}", :query => query)
+  end
+
+  def get(username, repo)
+    request_one(:path => "/show/#{username}/#{repo}")
   end
 
   def search(query)
     request_many(:path => "/search/#{query}")
   end
 
+  def unwatch(username, repo)
+    request_one(:path => "/unwatch/#{username}/#{repo}")
+  end
+
+  def update(username, repo, data)
+    query = {}
+    data.each do |key, value|
+      query["values[#{key}]"] = value
+    end
+    request_one(:method => :post, :path => "/show/#{username}/#{repo}", :query => query)
+  end
+
+  def watch(username, repo)
+    request_one(:path => "/watch/#{username}/#{repo}")
+  end
+
 end
 
 gothub = GotHub.new
-p gothub.repos.all('geemus')
 p gothub.repos.get('geemus', 'fog')
