d71a2157ccadc1f6c50d4fdfa4f1d6665c0b8994
geemus <geemus@gmail.com>
2011-05-23 18:50:45 -0700

    tweaks after latest workout

--- circuit.rb
+++ circuit.rb
@@ -6,27 +6,27 @@ srand
 @exercises = {
   :abs    => [
     '10x bosu situps',
-    '4x leg raises',
+    '3x 10s leg raises',
     '2x2 turkish getups'
   ],
   :cardio => [
+    '10x10 tactical lunge',
     '5x5 snatches',
     '30s jump rope',
     '30s rowing'
   ],
   :gymnastics => [
     '2x back lever',
-    '2x planche'
+    '2x 10s planche'
   ],
   :lower  => [
     '5x5 squat',
     '5x5 pistols',
-    '5x5 calf press',
-    '5x5 tactical lunge'
+    '5x5 calf press'
   ],
   :upper  => [
-    '2x handstand pushup, 4x pull up',
-    '5x push up, 5x full body row'
+    '2x handstand pushup, 5x pull up',
+    '5x push up, 10x full body row'
   ]
 }
 
