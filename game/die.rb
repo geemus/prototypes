6a0543b615aa22bcf3b99e56d513e860edab0a5b
geemus <geemus@gmail.com>
2011-06-26 16:43:43 -0700

    [game] add rolling multiple die

--- game/die.rb
+++ game/die.rb
@@ -5,12 +5,17 @@ class Die
     @sides = sides
   end
 
-  def roll
-    rand(@sides) + 1
+  def roll(quantity = 1)
+    total = 0
+    quantity.times do |i|
+      total += rand(@sides) + 1
+    end
+    total
   end
-  
+
 end
 
 10.times do
   p Die.new(6).roll
+  p Die.new(6).roll(2)
 end
