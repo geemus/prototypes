63201028846b5302e9455e00361c1050eb7bf1d4
geemus <geemus@gmail.com>
2011-06-27 08:41:39 -0700

    [game] reworking deck to be more flexible

--- game/deck.rb
+++ game/deck.rb
@@ -1,4 +1,40 @@
+# separate draw/discard into two separate decks
+# add push/pop shift/unshift as primary operations (each taking one or more items)
+
 class Deck
+
+  def initialize(cards=[])
+    srand
+    @cards = cards
+  end
+
+  def pop(count=1)
+    popped_cards = []
+    count.times do
+      popped_cards << @cards.pop
+    end
+    popped_cards
+  end
+
+  def push(cards)
+    [*cards].each do |card|
+      @cards.push(card)
+    end
+    cards
+  end
+
+  def shuffle
+    @cards = @cards.sort_by {|x,y| rand(2) - 1}
+  end
+
+end
+
+p deck = Deck.new([1,2,3])
+p deck.shuffle
+p hand = deck.pop(2)
+p deck.push(hand.pop)
+
+class LegacyDeck
 
   def initialize
     srand
@@ -31,5 +67,5 @@ class Deck
 
 end
 
-deck = Deck.new
-p deck.draw(5)
+#deck = Deck.new
+#p deck.draw(5)
