b249ed521191b6f6e706e40a472b166068d2f6ad
geemus <geemus@gmail.com>
2011-08-18 16:21:39 -0500

    [help] a little brainstorming

---
+++ help.rb
@@ -0,0 +1,16 @@
+# helped(
+#   :description => 'does that',
+#   :method      => 'this',
+#   :arguments   => [
+#     :first  => '[String] a string',
+#     :second => '[Integer]
+#   ]
+# )
+
+# helped do
+#   method 'this'
+#   description 'does that'
+#   argument :first, String, 'a string'
+#   argument :second, Integer, 'an Integer'
+#   argument :third, Hash, 'an options hash', {}
+# end
