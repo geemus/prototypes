95e982b550e5652a751895fd82c3237b5edd825c
geemus <geemus@gmail.com>
2011-09-13 20:52:48 -0500

    [stretch] basic stretches with audio queues and visual timers

---
+++ stretch.rb
@@ -0,0 +1,25 @@
+STDOUT.sync = true
+
+def stretch(name, duration = 120)
+  print("#{name}: ")
+  `say #{name}`
+  interval = (duration.to_f / 26.0)
+  26.times do |i|
+    print('abcdefghijklmnopqrstuvwxyz'[i..i])
+    sleep(interval)
+  end
+  print("\n")
+end
+
+# splits, 2 minutes each
+[
+  'pancake split',
+  'middle split',
+  'left split',
+  'right split'
+].each {|name| stretch(name, 120)}
+
+# bridge, 1 minute (for now)
+[
+  'bridge'
+].each {|name| stretch(name, 60)}
