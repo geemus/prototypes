e4caa691abc72d517b4b7b460ae0b3b58539fc9d
geemus <geemus@gmail.com>
2011-10-22 11:01:39 -0500

    [stretch] simplify and add short list for today

--- stretch.rb
+++ stretch.rb
@@ -1,27 +1,24 @@
 STDOUT.sync = true
 
-def stretch(name, duration = 120)
-  print("#{name}:\n  ")
-  `say -v victoria #{name}`
-  interval = (duration.to_f / 26.0)
-  26.times do |i|
-    print('abcdefghijklmnopqrstuvwxyz'[i..i])
-    sleep(interval)
+def stretches(names, duration = 120)
+  [*names].each do |name|
+    print("#{name}:\n  ")
+    `say -v victoria #{name}`
+    interval = (duration.to_f / 26.0)
+    26.times do |i|
+      print('abcdefghijklmnopqrstuvwxyz'[i..i])
+      sleep(interval)
+    end
+    print("\n")
   end
-  print("\n")
 end
 
 # splits, 2 minutes each
-[
-  'pancake split',
-  'middle split',
-  'left split',
-  'right split'
-].each {|name| stretch(name, 120)}
+#stretches(['pancake split', 'middle split', 'left split', 'right split'])
 
 # bridge, 1 minute (for now)
-[
-  'bridge'
-].each {|name| stretch(name, 60)}
+#stretches('bridge', 60)
+
+stretches(['middle split', 'pike stretch', 'pancake split'])
 
 `say -v victoria finish`
