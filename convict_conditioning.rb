cd7bfa488146ab887a9e2642848084d414caaf57
geemus <geemus@gmail.com>
2011-12-04 14:31:44 -0600

    [convict_conditioning] starting to hash out notes

---
+++ convict_conditioning.rb
@@ -0,0 +1,10 @@
+exercises = {
+  :push_up          => [:wall, :incline, :kneeling, :half, :full, :close, :uneven, :half_one_arm, :lever, :one_arm],
+  :squat            => [:shoulderstand, :jackknife, :supported, :half, :full, :close, :uneven, :half_one_leg, :assisted_one_leg, :one_leg],
+  :pull_up          => [:vertical, :horizontal, :jackknife, :half, :full, :close, :uneven, :half_one_arm, :assisted_one_arm, :one_arm],
+  :leg_raise        => [:knee_tuck, :flat_knee, :flat_bent, :flat_frog, :flat_straight, :hanging_knee, :hanging_bent, :hanging_frog, :hanging_partial_straight, :hanging_straight],
+  :bridge           => [:short, :straight, :angled, :head, :half, :full, :walk_walk_down, :wall_walk_up, :closing, :stand_to_stand],
+  :handstand_pushup => [:wall_headstand, :crow_stand, :wall_handstand, :half, :full, :close, :uneven, :half_one_hand, :lever, :one_arm]
+}
+
+p exercises
