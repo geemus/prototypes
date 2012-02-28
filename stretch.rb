fd4b80599adc9ef293f9cee64b0dcc8178b94301
geemus <geemus@gmail.com>
2012-02-28 13:23:44 -0600

    [stretch] cleanup/fixes

--- stretch.rb
+++ stretch.rb
@@ -8,32 +8,34 @@ end
 def stretches(stretches)
   say('breeth')
   print("\n")
-  keys = stretches.keys + ['stretch']
-  name_length = keys.map {|name| "#{name}:".length}.max
-  print("\r  #{'stretch:'.ljust(name_length)} \e[100m \e[0m\e[37m\e[47m#{'*' * 60}\e[0m\e[100m \e[0m\n")
-  [
-    ["bridge",      60],
-    ["pike",        60],
-    ["left couch",  60],
-    ["right couch", 60],
-    ["left",        60],
-    ["right",       60],
-    ["cobbler",     60],
-    ["pancake",     60],
-    ["middle",      60]
-  ].each do |name, duration|
+  stretches = stretches.map {|name, duration| ["#{name}:", duration]}
+  name_length = (stretches.map {|name, duration| name.length} + ["stretch:".length]).max
+  print("\r  #{'stretch:'.rjust(name_length)} \e[47m\e[90m|\e[37m#{'*' * 60}\e[0m\e[47m\e[90m|\e[0m")
+  print("\n\n")
+  stretches.each do |name, duration|
+    print("\r  #{name.rjust(name_length)} \e[47m\e[90m|\e[0m#{' ' * 60}\e[47m\e[90m|\e[0m")
     say(name)
-    sleep(2) # pause for setup/transition
+    sleep(3) # pause for setup/transition
     interval = (duration.to_f / 60.0)
-    name += ':'
     60.times do |i|
       i += 1
       completed, remaining = ('*' * i), (' ' * (60 - i))
-      print("\r  #{name.ljust(name_length)} \e[100m \e[0m\e[37m\e[47m#{completed}\e[0m#{remaining}\e[100m \e[0m")
+      print("\r  #{name.rjust(name_length)} \e[47m\e[90m|\e[37m#{completed}\e[0m#{remaining}\e[47m\e[90m|\e[0m")
       sleep(interval)
     end
-    print("\n")
+    print("\n\n")
   end
-  print("\n")
   say('relax')
 end
+
+stretches([
+  ["bridge",      60],
+  ["pike",        60],
+  ["couch left",  60],
+  ["couch right", 60],
+  ["left",        60],
+  ["right",       60],
+  ["cobbler",     60],
+  ["pancake",     60],
+  ["middle",      60]
+])
