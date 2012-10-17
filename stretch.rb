bae31121b00f7baf58dbc4d90522cb57d1ecbd39
geemus <geemus@gmail.com>
2012-10-16 20:06:55 -0500

    [stretch] update to support espeak as well

--- stretch.rb
+++ stretch.rb
@@ -1,8 +1,16 @@
+require 'open3'
+
 STDOUT.sync = true
 srand
 
 def say(string)
-  `say -v victoria #{string}`
+  if !`which say`.empty?
+    `say -v victoria #{string}`
+  elsif !`which espeak`.empty?
+    Open3.capture3("espeak #{string} &>/dev/null")
+  else
+    puts("Unknown speak synthesizer (neither `espeak` nor `say` is available).")
+  end
 end
 
 def stretches(stretches)
