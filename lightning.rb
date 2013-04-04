848fefab061ba8973fe76ae17436870c7b42f8fe
geemus <geemus@gmail.com>
2013-04-04 11:56:14 -0500

    [lightening] add first pass at a lightning talk timer

---
+++ lightning.rb
@@ -0,0 +1,27 @@
+STDOUT.sync = true
+
+def say(string)
+  if !`which say`.empty?
+    `say -v victoria #{string}`
+  elsif !`which espeak`.empty?
+    Open3.capture3("espeak #{string} &>/dev/null")
+  else
+    puts("Unknown speak synthesizer (neither `espeak` nor `say` is available).")
+  end
+end
+
+say('lightning')
+
+start = Time.now
+
+while true
+  elapsed = (Time.now - start).to_i
+  minutes = (elapsed / 60).to_s.rjust(2, "0")
+  seconds = (elapsed % 60).to_s.rjust(2, "0")
+  puts("#{minutes}:#{second}")
+  if elapsed > 300
+    break
+  end
+end
+
+say('thunder')
