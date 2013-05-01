dd9566be3269f1c65f837dadccaf3b99ffdf350a
geemus <geemus@gmail.com>
2013-04-30 21:32:52 -0500

    [meditate] add simple, speaking meditation timer

---
+++ meditate.rb
@@ -0,0 +1,32 @@
+require 'formatador'
+require 'open3'
+
+STDOUT.sync = true
+srand
+
+def say(string)
+  if !`which say`.empty?
+    `say -v victoria #{string}`
+  elsif !`which espeak`.empty?
+    Open3.capture3("espeak #{string} &>/dev/null")
+  else
+    puts("Unknown speak synthesizer (neither `espeak` nor `say` is available).")
+  end
+end
+
+start = Time.now
+
+while true
+  elapsed = (Time.now - start).to_i
+  minutes = elapsed / 60
+  if (minutes % 5 == 0)
+    unless warned
+      say(minutes)
+      warned = true
+    end
+  else
+    warned = false
+  end
+  seconds = (elapsed % 60).to_s.rjust(2, "0")
+  Formatador.redisplay("#{minutes.to_s.rjust(2, "0")}:#{seconds}")
+end
