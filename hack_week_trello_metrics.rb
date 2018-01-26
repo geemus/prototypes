c0ccc6f878efc1e89d0a036b24489f082a32124f
geemus <geemus@gmail.com>
2018-01-26 14:14:34 -0600

    [hack_week] cleanup

--- hack_week_trello_metrics.rb
+++ hack_week_trello_metrics.rb
@@ -19,12 +19,13 @@ board_id = "s7h2HGPk"
 ideas_list_id = "5a66645e93062e9fe9582cbe"
 experiments_list_id = "5a666478f1d8ff3c034f1189"
 
-members = request("/1/boards/#{board_id}/members")
-ideas_cards = request("/1/lists/#{ideas_list_id}/cards")
+members_count = request("/1/boards/#{board_id}/members").count
+ideas_count = request("/1/lists/#{ideas_list_id}/cards").count
 experiments_cards = request("/1/lists/#{experiments_list_id}/cards")
+experiments_count = experiments_cards.count
 
-experimenters = experiments_cards.map do |card|
+experimenters_count = experiments_cards.map do |card|
   request("/1/cards/#{card["id"]}/members").count
 end.reduce(:+)
 
-puts "Members: #{members.count} - Potential Ideas: #{ideas_cards.count} - Active Experiments: #{experiments_cards.count} - Experimenters: #{experimenters}"
+puts "Members: #{members_count} - Experimenters: #{experimenters_count} - Potential Ideas: #{ideas_count} - Active Experiments: #{experiments_count}"
