8085bd43716748f6304c72e81413a520fab8f13b
geemus <geemus@gmail.com>
2018-01-29 11:42:51 -0600

    [hack-week] dedup experimenters and enumerate inactive members

--- hack_week_trello_metrics.rb
+++ hack_week_trello_metrics.rb
@@ -20,14 +20,20 @@ ideas_list_id = "5a66645e93062e9fe9582cbe"
 experiments_list_id = "5a666478f1d8ff3c034f1189"
 
 invitees_count = 145
-members_count = request("/1/boards/#{board_id}/members").count
+members = request("/1/boards/#{board_id}/members").map {|member| member["username"]}
+members_count = members.count
 ideas_count = request("/1/lists/#{ideas_list_id}/cards").count
 experiments_cards = request("/1/lists/#{experiments_list_id}/cards")
 experiments_count = experiments_cards.count
 
-experimenters_count = experiments_cards.map do |card|
-  request("/1/cards/#{card["id"]}/members").count
-end.reduce(:+)
+experimenters = []
+experiments_cards.each do |card|
+  experimenters.concat(
+    request("/1/cards/#{card["id"]}/members").map {|member| member["username"]}
+  )
+end
+experimenters.uniq!
+experimenters_count = experimenters.count
 
 #Members: #{members_count} - Experimenters: #{experimenters_count} - Potential Ideas: #{ideas_count} - Active Experiments: #{experiments_count}"
 
@@ -38,3 +44,4 @@ ideas_ratio = (ideas_count.to_f / members_count.to_f).round(1)
 puts "#{members_count} members | #{members_percent}% of invitees"
 puts "#{ideas_count} potential ideas | #{ideas_ratio} ideas per member"
 puts "#{experiments_count} active experiments | #{experimenters_percent}% members active"
+puts "Inactive: [#{(members - experimenters).join(", ")}]"
