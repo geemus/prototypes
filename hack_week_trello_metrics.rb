4989ba5e0ba4e0472308340b07822b5d6cd3a9fd
geemus <geemus@gmail.com>
2018-01-31 14:14:23 -0600

    [hack-week] count unique authors

--- hack_week_trello_metrics.rb
+++ hack_week_trello_metrics.rb
@@ -3,16 +3,16 @@ require "json"
 
 def connection
   @connection ||= Excon.new(
-    "https://api.trello.com",
-    query: {
-      key: ENV["TRELLO_KEY"],
-      token: ENV["TRELLO_TOKEN"]
-    }
+    "https://api.trello.com"
   )
 end
 
-def request(path)
-  JSON.parse(connection.get(path: path).body)
+def request(path, query = {})
+  query = {
+    key: ENV["TRELLO_KEY"],
+    token: ENV["TRELLO_TOKEN"]
+  }.merge(query)
+  JSON.parse(connection.get(path: path, query: query).body)
 end
 
 board_id = "s7h2HGPk"
@@ -26,6 +26,19 @@ ideas_count = request("/1/lists/#{ideas_list_id}/cards").count
 experiments_cards = request("/1/lists/#{experiments_list_id}/cards")
 experiments_count = experiments_cards.count
 
+ideators = []
+board_actions = request(
+    "/1/boards/#{board_id}/actions",
+    {
+      filter: "createCard"
+    }
+  )
+board_actions.each do |action|
+  ideators << action["memberCreator"]["username"]
+end
+ideators.uniq!
+ideators_count = ideators.count
+
 experimenters = []
 experiments_cards.each do |card|
   experimenters.concat(
@@ -35,13 +48,11 @@ end
 experimenters.uniq!
 experimenters_count = experimenters.count
 
-#Members: #{members_count} - Experimenters: #{experimenters_count} - Potential Ideas: #{ideas_count} - Active Experiments: #{experiments_count}"
-
 members_percent = (members_count.to_f / invitees_count.to_f * 100).round(1)
 experimenters_percent = (experimenters_count.to_f / members_count.to_f * 100).round(1)
 ideas_ratio = (ideas_count.to_f / members_count.to_f).round(1)
 
-puts "#{members_count} members | #{members_percent}% of invitees"
-puts "#{ideas_count} potential ideas | #{ideas_ratio} ideas per member"
-puts "#{experiments_count} active experiments | #{experimenters_percent}% members active"
-puts "Inactive: [#{(members - experimenters).join(", ")}]"
+puts "#{members_count} board members | #{members_percent}% of invitees"
+puts "#{ideas_count} potential idea cards from #{ideators_count} members | #{ideas_ratio} ideas per member"
+puts "#{experiments_count} active experiment cards | #{experimenters_count} experiment members | #{experimenters_percent}% board members"
+puts "Uncommitted Board Members: [#{(members - experimenters).join(", ")}]"
