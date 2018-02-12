f66e2c0cd99540d17f97a65873cdfff4445651d6
geemus <geemus@gmail.com>
2018-02-12 15:32:01 -0600

    [hack-week-megamix] add index file

--- hack_week_megamix.rb
+++ hack_week_megamix.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+require 'json'
 require 'rmagick'
 require 'shellwords'
 
@@ -33,11 +34,33 @@ raw_paths.each do |path|
   image.write(image_path)
 
   # generate a 2 second video from the image, with a fake audio source padded to the 2 second duration
-  `ffmpeg -y -framerate 30 -loop 1 -i #{image_path.shellescape} -f lavfi -i anullsrc=32000 -af apad -c:a aac -t 2 -c:v libx264 -t 2 -movflags +faststart -pix_fmt yuv420p -shortest -vsync cfr #{image_video_path.shellescape}`
+  `ffmpeg -y -framerate 30 -loop 1 -i #{image_path.shellescape} -f lavfi -i anullsrc=cl=stereo:r=32k -af apad -b:a 128k -b:v 584k -c:a aac -c:v libx264 -t 2 -movflags +faststart -pix_fmt yuv420p -shortest -t 2 -vsync cfr #{image_video_path.shellescape}`
 
   file_paths << image_video_path
   file_paths << path
 end
+
+offset = 12 # intro video length
+index_data = "00:00:00 hw-intro\n"
+
+raw_paths.each do |path|
+  json = `ffprobe -i #{path.shellescape} -hide_banner -print_format json -show_format -v quiet`
+  duration = JSON.parse(json)['format']['duration'].to_f.round
+  team_name = File.basename(path, ".mp4")
+
+  hours = offset / 3600
+  minutes = (offset % 3600) / 60
+  seconds = offset % 60
+  timestamp = [hours, minutes, seconds].map {|time| time.to_s.ljust(2,'0')}.join(':')
+  index_data << "#{timestamp} #{team_name}\n"
+  offset += duration + 2 # 2 is title slide length
+end
+
+index_path = "./index.txt"
+File.open(index_path, 'w') do |file|
+  file.write index_data
+end
+
 
 # convert to intermediary format, so that everything is in the same format before concat
 # FIXME: theoretically, I think the image->video process could just output in compatible format, but after many tries I couldn't get the raw files to concat and intermediary works
