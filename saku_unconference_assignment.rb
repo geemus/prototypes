a1717e5c721671e46378016209d80401d9de458f
geemus <geemus@gmail.com>
2019-04-22 08:28:30 -0500

    stage old saku trello assigmnent changes

--- saku_unconference_assignment.rb
+++ saku_unconference_assignment.rb
@@ -7,48 +7,42 @@ def connection
   )
 end
 
-def request(path, query = {})
+def request(method, path, query = {})
   query = {
     key: ENV["TRELLO_KEY"],
     token: ENV["TRELLO_TOKEN"]
   }.merge(query)
-  JSON.parse(connection.get(path: path, query: query).body)
+  JSON.parse(connection.request(method: method, path: path, query: query).body)
 end
 
-# { id: { idMembers: [...], idMembersVoted: [...], name: "..." } }
+board_id = "ZTunu3ww" # real
+# board_id = "SMrfVa2j" # scratch
 
-sessions = {}
+lists = request(:get, "/1/boards/#{board_id}/lists/")
+proposed_list_id = lists.detect {|list| list['name'] == 'Proposed'}['id']
+session_list_ids = [
+  lists.detect {|list| list['name'] == 'Session 1'}['id'],
+  lists.detect {|list| list['name'] == 'Session 2'}['id'],
+  lists.detect {|list| list['name'] == 'Session 3'}['id'],
+  lists.detect {|list| list['name'] == 'Session 4'}['id']
+]
+overflow_list_id = lists.detect {|list| list['name'] == 'Overflow'}['id']
 
-board_id = "ZTunu3ww"
+proposed_cards = request(:get, "/1/lists/#{proposed_list_id}/cards")
+# sort by votes, reverse to get descending order
+sorted_cards = proposed_cards.sort_by {|card| [card['idMembersVoted'].count, rand]}.reverse
 
-#puts request("/1/boards/#{board_id}/lists/")
-proposed_list_id = "5afb4079653494f2284768e3"
-proposed_cards = request("/1/lists/#{proposed_list_id}/cards")
-
-proposed_cards.each do |card|
-  #next unless card['badges']['votes'] >= 3
-  sessions[card['id']] = {
-    idMembers:      card['idMembers'],
-    idMembersVoted: card['idMembersVoted'],
-    name:           card['name']
-  }
-end
-
-slots = [[],[],[],[]]
-
-# sort from most votes to least votes
-sorted_keys = sessions.keys.sort_by {|key| [sessions[key][:idMembersVoted].count, rand]}.reverse
-9.times do # just 9 times, as one room will be podcasting throughout
-  slot_keys = sorted_keys.shift(4)
-  4.times do |i|
-    session = sessions[slot_keys[i]]
-    slots[i] << "(#{session[:idMembersVoted].count}) #{session[:name]}"
+# round robin to fill sessions
+7.times do |x|
+  cards = sorted_cards.shift(4)
+  4.times do |y|
+    request(:put, "/1/cards/#{cards[y]['id']}", { idList: session_list_ids[y] })
   end
 end
-4.times do |i|
-  puts "#{i}: #{slots[i]}"
-end
 
-puts "x: #{sessions.select {|key,value| sorted_keys.include?(key)}.map {|key, value| "(#{value[:idMembersVoted].count}) #{value[:name]}"}}"
+# place remaining cards in overflow list
+sorted_cards.each do |card|
+  request(:put, "/1/cards/#{card['id']}", { idList: overflow_list_id })
+end
 
 puts "#{Time.now}"
