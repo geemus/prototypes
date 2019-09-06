f9476d8d68dd7da6838a4f8d4c8cffc3b4e01fa0
geemus <geemus@gmail.com>
2019-09-06 13:44:08 -0500

    add bmw stats tracking

---
+++ bmw_trello_metrics.rb
@@ -0,0 +1,57 @@
+require "excon"
+require "json"
+
+def connection
+  @connection ||= Excon.new(
+    "https://api.trello.com"
+  )
+end
+
+def request(path, query = {})
+  query = {
+    key: ENV["TRELLO_KEY"],
+    token: ENV["TRELLO_TOKEN"]
+  }.merge(query)
+  JSON.parse(connection.get(path: path, query: query).body)
+end
+
+board_id = "adK1aOmP"
+
+members = request("/1/boards/#{board_id}/members").map {|member| member["username"]}
+members_count = members.count
+
+cards = request("/1/boards/#{board_id}/cards")
+cards_count = cards.count
+
+creators = []
+board_actions = request(
+    "/1/boards/#{board_id}/actions",
+    {
+      filter: "createCard"
+    }
+  )
+board_actions.each do |action|
+  creators << action["memberCreator"]["username"]
+end
+creators.uniq!
+creators_count = creators.count
+
+joiners = []
+cards.each do |card|
+  joiners.concat(
+    request("/1/cards/#{card["id"]}/members").map {|member| member["username"]}
+  )
+end
+joiners.uniq!
+joiners_count = joiners.count
+
+uncommitted_members = members - joiners
+#members_percent = (members.count.to_f / invitees_count.to_f * 100).round(1)
+joiners_percent = (joiners_count.to_f / members_count.to_f * 100).round(1)
+cards_ratio = (cards_count.to_f / members_count.to_f).round(1)
+
+puts "#{members_count} board members"
+puts "#{cards_count} cards from #{creators_count} members | #{cards_ratio} ideas per member"
+puts "#{cards_count} learning opportunities"
+puts "#{joiners_count} joining members | #{joiners_percent}% board members"
+puts "#{uncommitted_members.count} Uncommitted Board Members: [#{uncommitted_members.join(", ")}]"
