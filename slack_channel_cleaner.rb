1f52a7c645c17c9a106a14204ee661a63da68539
geemus <geemus@gmail.com>
2019-10-18 11:39:18 -0500

    refine slack cleaner checking for messages by type

--- slack_channel_cleaner.rb
+++ slack_channel_cleaner.rb
@@ -13,7 +13,7 @@ channel_list_response = connection.get({
   query: {
     cursor: cursor,
     exclude_archived: true,
-    limit: 100,
+    limit: 200,
     token: ENV['SLACK_HEROKU_CHANNEL_CLEANER'],
     types: 'public_channel'
   }
@@ -33,20 +33,23 @@ JSON.parse(channel_list_response.body)['channels'].each do |channel|
     path: 'api/conversations.history',
     query: {
       channel: channel['id'],
-      limit: 1,
+      limit: 10,
       token: ENV['SLACK_HEROKU_CHANNEL_CLEANER']
     }
   })
-  messages = JSON.parse(channel_response.body)['messages']
-  unless messages.empty?
-    channels[channel['name']][:ts] = Time.at(messages.first['ts'].to_f).utc
+  channel_response_json = JSON.parse(channel_response.body)
+  messages = channel_response_json['messages']
+  # messages also includes things like joins/leaves/etc, we just want actual messages
+  if last_message = messages.detect {|message| message['type'] == 'message' && (!message.has_key?('subtype') || message['subtype'] == 'bot_message')}
+    channels[channel['name']][:ts] = Time.at(last_message['ts'].to_f).utc
   end
+  channels[channel['name']][:has_more] = channel_response_json['has_more']
 end
 puts
 
 channels.each do |key, channel|
-  next unless channel.has_key?(:ts) && channel[:ts] < STALE
-  next unless channel[:created] < STALE
+  # skip unless the last message was more than 6 months ago or there are no messages
+  next unless channel.fetch(:ts, Time.at(0.0).utc) < STALE
   print "STALE <=> #{channel} <=> Archive (y/n)? "
   if $stdin.gets.strip == "y"
     archive_response = connection.post({
@@ -60,4 +63,5 @@ channels.each do |key, channel|
   end
 end
 
+puts "#{channels.values.last[:created]} <=> #{channels.values.last[:ts]}"
 puts "CURSOR: #{ARGV.first} || NEXT: #{JSON.parse(channel_list_response.body)['response_metadata']['next_cursor']}"
