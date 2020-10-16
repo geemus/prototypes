badbb8769ce99ec06f63e75483aa0d2cf93123b7
geemus <geemus@gmail.com>
2020-10-16 15:16:47 -0500

    cleanup covid output

--- covid.rb
+++ covid.rb
@@ -1,24 +1,24 @@
 require 'excon'
 
+COUNTY = "Johnson County,IA"
+
 TARGETS = {
   confirmed: "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv",
   deaths: "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_deaths_usafacts.csv"
 }
 
+puts "Covid Data: " << COUNTY
+
 TARGETS.each do |key, value|
   data = Excon.get(value).body
 
   if key == :confirmed
-    puts "HEADERS"
-    puts data.split("\r\n").first
-    puts
+    puts "DATES: " << data.split("\r\n").first.split(",").last(10).join(",")
   end
 
-  puts key.to_s.upcase
   data.split("\r\n").each do |line|
-    next unless line.include?("Johnson County,IA")
-    puts line
+    next unless line.include?(COUNTY)
+    puts "#{key.to_s.upcase}: " << line.split(",").last(10).join(",")
     break
   end
-  puts
 end
