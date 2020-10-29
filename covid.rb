30377bec7e655192af5127bb7d8743050493ca0d
geemus <geemus@gmail.com>
2020-10-29 17:02:40 -0500

    [covid] pull more data from covidactnow

--- covid.rb
+++ covid.rb
@@ -30,10 +30,19 @@ response = Excon.get(
   query: { 'apiKey' => ENV['COVID_ACT_NOW'] }
 )
 data = JSON.parse(response.body)
-timeseries = data['metricsTimeseries'].last(10)
-dates = timeseries.map {|a| a['date'].gsub('-','/')}.join(',')
-positivities = timeseries.map {|a| a['testPositivityRatio'].round(2)}.join(',')
 
-puts "TEST POSITIVITY"
-puts "DATES: #{dates}"
-puts "RATIO: #{positivities}"
+actuals = data['actualsTimeseries'].last(10)
+actuals_dates = actuals.map {|a| a['date'].gsub('-','/')}.join(',')
+cases = actuals.map {|a| a['cases']}.join(',')
+deaths = actuals.map {|a| a['deaths']}.join(',')
+
+metrics = data['metricsTimeseries'].last(10)
+metrics_dates = metrics.map {|a| a['date'].gsub('-','/')}.join(',')
+positivities = metrics.map {|a| a['testPositivityRatio'].round(2)}.join(',')
+
+puts "covidactnow API"
+puts "ACTUALS DATES: #{actuals_dates}"
+puts "CASES: #{cases}"
+puts "DEATHS: #{deaths}"
+puts "METRICS DATES: #{metrics_dates}"
+puts "TEST POSITIVITY RATIO: #{positivities}"
