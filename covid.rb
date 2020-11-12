f6e9c75b7768fdbccb3f3c61c8d2ab9ca4f65c6d
geemus <geemus@gmail.com>
2020-11-12 13:23:08 -0600

    [covid] simplify to only use covidactnow data

--- covid.rb
+++ covid.rb
@@ -1,29 +1,7 @@
 require 'excon'
 require 'json'
 
-COUNTY = "Johnson County,IA"
-FIPS = "19103"
-
-TARGETS = {
-  confirmed: "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv",
-  deaths: "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_deaths_usafacts.csv"
-}
-
-puts "Covid Data: " << COUNTY
-
-TARGETS.each do |key, value|
-  data = Excon.get(value).body
-
-  if key == :confirmed
-    puts "DATES: " << data.split("\r\n").first.split(",").last(10).join(",")
-  end
-
-  data.split("\r\n").each do |line|
-    next unless line.include?(COUNTY)
-    puts "#{key.to_s.upcase}: " << line.split(",").last(10).join(",")
-    break
-  end
-end
+FIPS = "19103" # Johnson County, IA
 
 response = Excon.get(
   "https://api.covidactnow.org/v2/county/#{FIPS}.timeseries.json",
