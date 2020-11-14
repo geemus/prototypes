b63603d836e9c86614bca35ab855b3467fdf9603
geemus <geemus@gmail.com>
2020-11-14 11:39:30 -0600

    [covid] tabular output for readability

--- covid.rb
+++ covid.rb
@@ -11,17 +11,17 @@ response = Excon.get(
 data = JSON.parse(response.body)
 
 actuals = data['actualsTimeseries'].last(10)
-actuals_dates = actuals.map {|a| a['date'].gsub('-','/')}.join(',')
-cases = actuals.map {|a| a['cases']}.join(',')
-deaths = actuals.map {|a| a['deaths']}.join(',')
+actuals_dates = actuals.map {|a| a['date'].split('-')[1,2].join('/')}.join(', ')
+cases = actuals.map {|a| a['cases'].to_s.rjust(5,' ')}.join(', ')
+deaths = actuals.map {|a| a['deaths'].to_s.rjust(5,' ')}.join(', ')
 
 metrics = data['metricsTimeseries'].last(10)
-metrics_dates = metrics.map {|a| a['date'].gsub('-','/')}.join(',')
-positivities = metrics.map {|a| a['testPositivityRatio'] && a['testPositivityRatio'].round(2)}.join(',')
+metrics_dates = metrics.map {|a| a['date'].split('-')[1,2].join('/')}.join(', ')
+positivities = metrics.map {|a| a['testPositivityRatio'] && a['testPositivityRatio'].round(2).to_s.rjust(5,' ') || '     '}.join(', ')
 
 puts "covidactnow API"
-puts "ACTUALS DATES: #{actuals_dates}"
-puts "CASES: #{cases}"
+puts "DATES:  #{actuals_dates}"
+puts "CASES:  #{cases}"
 puts "DEATHS: #{deaths}"
-puts "METRICS DATES: #{metrics_dates}"
-puts "TEST POSITIVITY RATIO: #{positivities}"
+puts "DATES:  #{metrics_dates}"
+puts "RATIO:  #{positivities}"
