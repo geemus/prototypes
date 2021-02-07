9d5f0dc00126d76a731f379b7402eadee524c5cb
geemus <geemus@gmail.com>
2021-02-07 07:49:48 -0600

    [covid] experiment with vaccinations, but leave out nils for now

--- covid.rb
+++ covid.rb
@@ -14,14 +14,16 @@ actuals = data['actualsTimeseries'].last(10)
 actuals_dates = actuals.map {|a| a['date'].split('-')[1,2].join('/')}.join(', ')
 cases = actuals.map {|a| a['cases'].to_s.rjust(5,' ')}.join(', ')
 deaths = actuals.map {|a| a['deaths'].to_s.rjust(5,' ')}.join(', ')
+#vaccines = actuals.map {|a| a['vaccinationsInitiated'].to_s.rjust(5,' ')}.join(', ')
+#vaccines = actuals.map {|a| a['vaccinationsCompleted'].to_s.rjust(5,' ')}.join(', ')
 
-metrics = data['metricsTimeseries'].last(10)
-metrics_dates = metrics.map {|a| a['date'].split('-')[1,2].join('/')}.join(', ')
-positivities = metrics.map {|a| a['testPositivityRatio'] && a['testPositivityRatio'].round(2).to_s.rjust(5,' ') || '     '}.join(', ')
+#metrics = data['metricsTimeseries'].last(10)
+#metrics_dates = metrics.map {|a| a['date'].split('-')[1,2].join('/')}.join(', ')
+#positivities = metrics.map {|a| a['testPositivityRatio'] && a['testPositivityRatio'].round(2).to_s.rjust(5,' ') || '     '}.join(', ')
 
 puts "covidactnow API"
-puts "DATES:  #{actuals_dates}"
-puts "CASES:  #{cases}"
-puts "DEATHS: #{deaths}"
-puts "DATES:  #{metrics_dates}"
-puts "RATIO:  #{positivities}"
+puts "   DATES: #{actuals_dates}"
+puts "   CASES: #{cases}"
+puts "  DEATHS: #{deaths}"
+#puts "   DATES: #{metrics_dates}"
+#puts "   RATIO: #{positivities}"
