f423c26e9d4f775f8e0a027dbab921adb0b4d5a6
geemus <geemus@gmail.com>
2022-10-05 14:57:46 -0500

    [kindle] cleanup formatting

--- extract_kindle_notes.rb
+++ extract_kindle_notes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 
 data = if !`which xclip`.empty?
@@ -15,15 +17,16 @@ puts "# #{title} by #{author}\n"
 doc.css('.kp-notebook-row-separator').each do |row|
   metadata = row.css('.kp-notebook-metadata').first.content
 
-  splitter = if metadata.include?("Location:")
-    "Location:"
-  elsif metadata.include?("Page:")
-    "Page:"
-  end
-  location = metadata.split(splitter).last[1..-1].gsub(',','')
+  splitter = if metadata.include?('Location:')
+               'Location:'
+             elsif metadata.include?('Page:')
+               'Page:'
+             end
+  location = metadata.split(splitter).last[1..-1].gsub(',', '')
 
   highlight = row.css('#highlight').first
   next unless highlight
+
   quote = highlight.inner_text
 
   puts "- #{quote} (#{location})"
