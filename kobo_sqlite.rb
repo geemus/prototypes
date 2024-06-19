b531db0dbc960fae92c621b4c4670902eb9b87be
geemus <geemus@gmail.com>
2024-06-19 18:44:51 +0000

    add todo/notes to kobo parser

--- kobo_sqlite.rb
+++ kobo_sqlite.rb
@@ -2,9 +2,12 @@
 
 require 'sqlite3'
 
+# TODO: copy database directly from kobo to tmp
+
 db = SQLite3::Database.new('/tmp/KoboReader.sqlite')
 
-# FIXME: ? bookmark annotations (if any)
+# FIXME: ? annotations (if any)
+# TODO: limit to relevant columns
 columns, *rows = db.execute2(
   'SELECT * from Bookmark INNER JOIN Content ON Bookmark.ContentID = Content.ContentID WHERE Bookmark.Type = "highlight" ORDER BY Bookmark.ContentID'
 )
@@ -14,6 +17,8 @@ bookmarks = rows.map { |row| Hash[columns.zip(row)] }
 title = nil
 chapter = nil
 
+# TODO: create a directory in tmp for output
+# TODO: write each book to it's own md file within dir
 bookmarks.each do |bookmark|
   if title != bookmark['BookTitle']
     title = bookmark['BookTitle']
@@ -26,7 +31,9 @@ bookmarks.each do |bookmark|
   end
   if chapter != bookmark['Title']
     chapter = bookmark['Title']
-    puts "## #{chapter}"
+    # assume that first highlight in the chapter is the title, so that we can include them
+    puts "## #{bookmark['Text']}"
+    next
   end
 
   text = bookmark['Text']
