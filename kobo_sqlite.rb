8c14d69d2b79342c98d0c043793d1d863bf649ce
geemus <geemus@gmail.com>
2024-06-19 19:40:55 +0000

    continuing to develop/refine kobo parsing

--- kobo_sqlite.rb
+++ kobo_sqlite.rb
@@ -1,42 +1,60 @@
 # frozen_string_literal: true
 
+require 'fileutils'
 require 'sqlite3'
 
-# TODO: copy database directly from kobo to tmp
+FileUtils.cp(
+  '/media/geemus/KOBOeReader/.kobo/KoboReader.sqlite',
+  '/tmp/KoboReader.sqlite'
+)
 
 db = SQLite3::Database.new('/tmp/KoboReader.sqlite')
 
 # FIXME: ? annotations (if any)
 # TODO: limit to relevant columns
 columns, *rows = db.execute2(
-  'SELECT * from Bookmark INNER JOIN Content ON Bookmark.ContentID = Content.ContentID WHERE Bookmark.Type = "highlight" ORDER BY Bookmark.ContentID'
+  'SELECT * from Bookmark INNER JOIN Content ON Bookmark.ContentID = Content.ContentID WHERE Bookmark.Type = "highlight" ORDER BY Bookmark.ContentID, Bookmark.ChapterProgress ASC, Bookmark.StartOffset ASC'
 )
 
 bookmarks = rows.map { |row| Hash[columns.zip(row)] }
 
 title = nil
 chapter = nil
+data = nil
 
-# TODO: create a directory in tmp for output
+FileUtils.mkdir_p('/tmp/kobo')
+
 # TODO: write each book to it's own md file within dir
+
+def write_file(title, data)
+  return if data.nil?
+
+  filename = title.gsub(/[^0-9A-Za-z.-]/, '_').gsub(/_{2,}/, '_')
+  path = "/tmp/kobo/#{filename}.md"
+  File.write(path, data)
+  puts "wrote #{path}"
+end
+
 bookmarks.each do |bookmark|
   if title != bookmark['BookTitle']
+    write_file(title, data)
     title = bookmark['BookTitle']
+    data = +''
     columns, *rows = db.execute2(
       "SELECT * from Content WHERE ContentID = '#{bookmark['VolumeID']}'"
     )
     book = Hash[columns.zip(rows.first)]
-    puts book
-    puts "# *#{title}* by #{book['Attribution']}"
+    data << "# *#{title}* by #{book['Attribution']}\n"
   end
   if chapter != bookmark['Title']
     chapter = bookmark['Title']
     # assume that first highlight in the chapter is the title, so that we can include them
-    puts "## #{bookmark['Text']}"
+    data << "## #{bookmark['Text']}\n"
     next
   end
 
   text = bookmark['Text']
-  text = text.split("\n").join("\n    ")
-  puts "- #{text}"
+  text = text.split("\n").join("\n    ") if text
+  data << "- #{text}\n"
 end
+write_file(title, data)
